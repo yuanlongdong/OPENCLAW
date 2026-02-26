c5f12f468529791752a23b27087cf7460e842072
yuanlongdong <97449554+yuanlongdong@users.noreply.github.com>
2026-02-26 18:44:28 +0800

    update hardhat.config.js for bsc testnet

--- hardhat.config.js
+++ hardhat.config.js
@@ -4,8 +4,9 @@ require('@nomicfoundation/hardhat-ethers');
 module.exports = {
   solidity: '0.8.24',
   networks: {
-    sepolia: {
-      url: process.env.RPC_URL || '',
+    bscTestnet: {
+      url: process.env.RPC_URL || 'https://bsc-testnet-rpc.publicnode.com',
+      chainId: 97,
       accounts: process.env.DEPLOYER_PRIVATE_KEY ? [process.env.DEPLOYER_PRIVATE_KEY] : []
     }
   }
